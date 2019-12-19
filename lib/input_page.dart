1b18e099fca54a3299bd3854d5adb14b38c5c38e
cbstodd <cbstodd@gmail.com>
2019-12-18 23:50:03 -0500

    Created Some custom widgets that allow for DRY practices using custom peramaters

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,7 @@
 import 'package:bmi_calculator/raven_theme_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -20,105 +21,124 @@ class _InputPageState extends State<InputPage> {
         child: Container(
           decoration: _ravenTheme.linearGradientBackground(),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      child: Text('Container 1'),
-                      height: 150,
-                      margin: EdgeInsets.fromLTRB(10.0, 20.0, 5.0, 10.0),
-                      padding: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        color: _ravenTheme.matteBlue,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5.0),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: CustomCardContainer(
+                        colorValue: _ravenTheme.matteBlue,
+                        cardChild: CustomCardChildColumn(
+                          customIcon: FontAwesomeIcons.mars,
+                          customText: 'Male',
+                          ravenTheme: _ravenTheme,
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      child: Text('Container 2'),
-                      height: 150,
-                      margin: EdgeInsets.fromLTRB(5.0, 20.0, 10.0, 10.0),
-                      padding: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        color: _ravenTheme.matteBlue,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5.0),
+                    Expanded(
+                      child: CustomCardContainer(
+                        colorValue: _ravenTheme.matteBlue,
+                        cardChild: CustomCardChildColumn(
+                          customIcon: FontAwesomeIcons.venus,
+                          customText: 'Female',
+                          ravenTheme: _ravenTheme,
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      child: Text('Container 2'),
-                      height: 300,
-                      margin: EdgeInsets.fromLTRB(10.0, 0, 10.0, 10.0),
-                      padding: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        color: _ravenTheme.matteBlue,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5.0),
-                        ),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: CustomCardContainer(
+                        colorValue: _ravenTheme.matteBlue,
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      child: Text('Container 1'),
-                      height: 150,
-                      margin: EdgeInsets.fromLTRB(10.0, 0, 5.0, 0),
-                      padding: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        color: _ravenTheme.matteBlue,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5.0),
-                        ),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: CustomCardContainer(
+                        colorValue: _ravenTheme.matteBlue,
                       ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      child: Text('Container 2'),
-                      height: 150,
-                      margin: EdgeInsets.fromLTRB(5.0, 0, 10.0, 0),
-                      padding: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        color: _ravenTheme.matteBlue,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5.0),
-                        ),
+                    Expanded(
+                      child: CustomCardContainer(
+                        colorValue: _ravenTheme.matteBlue,
                       ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class CustomCardChildColumn extends StatelessWidget {
+  final IconData customIcon;
+  final String customText;
+  final RavenTheme _ravenTheme;
+
+  const CustomCardChildColumn({
+    Key key,
+    RavenTheme ravenTheme,
+    @required this.customIcon,
+    @required this.customText,
+  })  : _ravenTheme = ravenTheme,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          customIcon,
+          size: 100.0,
+          color: _ravenTheme.purple,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          customText,
+          style: TextStyle(
+            color: _ravenTheme.offWhite,
+            fontSize: 30.0,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+// Custom Widget:
+class CustomCardContainer extends StatelessWidget {
+  final Color colorValue;
+  final Widget cardChild;
+
+  const CustomCardContainer({Key key, @required this.colorValue, this.cardChild}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: colorValue,
+        borderRadius: BorderRadius.all(
+          Radius.circular(5.0),
+        ),
       ),
     );
   }
